c00d763d3eaec6e10d4ca2c10ce3b3143ebdcfd5
KimYuRa2 <ura3118@naver.com>
2023-01-09 12:25:34 +0900

    ~6. event

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ name }}</div>
+  <div>{{ name2 }}</div>
+  <button class="btn btn-outline-primary" v-on:click="consoleLog">Click!</button>
+  <button class="btn btn-outline-primary" v-on:click="updateName">update</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default{
+    setup(){
+      const name = 'kossie corder2';  // 업데이트를 위해 자료형을 const => let으로 바꿈
+      let name2 = 'KYR';  // 업데이트를 위해 자료형을 const => let으로 바꿈
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      // const greeting = () => {
+      //   return 'hello!' + name;
+      // }
+
+      // const greetname = greeting(name);
+      
+      const consoleLog = () => {
+        console.log("hello world~~~~");
+      }
+
+      const updateName = () => {
+        name2 = 'update!!!'; 
+      }
+
+      return{ //return하는 변수들은 template 안에서 접근이 가능해짐!
+        name,
+        name2,
+        consoleLog,
+        updateName
+      }; 
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ /* css 코드 */
+ .name {
+  color: red;
+ }
 </style>
