c3fb9b0a2ac582bdd9855f910cc44fbd5a54c3cb
KimYuRa2 <ura3118@naver.com>
2023-01-11 01:07:56 +0900

    10. 리스트에 새로운 todo 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <input 
-   type="text" 
-   v-model = "name"
-  >
+  <div class="container">
 
-  <button 
-   class="btn btn-outline-primary" 
-   @click="onSubmit"
-  >
-    Click
-  </button>
+    <h2>To-Do List</h2>
 
+    <form
+      @submit.prevent = "onSubmit"
+      class="d-flex"
+    >
+      <div class="flex-grow-1 mr-2">
+        <input 
+          type="text" 
+          v-model = "todo"
+          class = "form-control"
+          placeholder="Type new to-do"
+        >
+      </div>
+
+      <div>
+        <button 
+          type="submit"
+          class="btn btn-outline-primary" 
+        >
+          Add
+        </button>
+      </div>
+    </form>
+
+
+    {{ todos }}
+
+  </div>
+  
+  
 </template>
 
 <script>
@@ -19,17 +40,24 @@
   export default{
     setup(){
 
-      const name = ref('kossie');
+      const todo = ref(''); // 처음 input박스는 공란('')으로 설정
+      const todos = ref([]); // 처음 todo리스트는 빈 배열으로 설정
 
       const onSubmit = () => {
-        console.log(name.value);
-      }
+        //console.log(todo.value);
+        //e.preventDefault(); // form을 submit하면, 화면을 reloading( refresh) 하는데, 이를 방지. => <form @sumit.prevent = "onSubmit"></form>로 대체함!!
+        todos.value.push({
+          id : Date.now(), // 유니크한 key를 만들어내기 위함.
+          subject : todo.value
+        });
+      };
 
 
 
       return{ //return하는 변수들은 template 안에서 접근이 가능해짐!
-        name,
-        onSubmit
+        todo,
+        todos,
+        onSubmit,
       }; 
     }
   }
@@ -39,6 +67,9 @@
  /* css 코드 */
  .name {
   color : blue;
+  background-color : yellow;
+ }
+ .hi{
   background-color : yellow;
  }
 </style>
