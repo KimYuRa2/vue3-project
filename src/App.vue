7dbd56c15d1197f7e2cfefadf1d950bdf7d305de
KimYuRa2 <ururur68@gmail.com>
2023-01-13 16:40:10 +0900

    17.  폼 컴포넌트 만들기 (TodoSimpleComponent.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,22 @@
 <template>
   <!--13. v-show : 처음부터 두개의 div를 렌더링한 후, style을 이용해서 나타나거나 감추도록 함 ( display: none; ) => toggle같은 것을 자주 해줄 때 유용함.-->
-  <div v-show = "toggle">true</div>
+  <!-- <div v-show = "toggle">true</div>
   <div v-show = "!toggle">false</div>
-  <button @click = "onToggle">Toggle</button>
+  <button @click = "onToggle">Toggle</button> -->
 
   <!--13. v-if : 처음부터 조건에 맞는 div만 렌더링한다. => 런타임동안 조건이 거의 바뀌지 않을 때 사용하면 유용함. -->
-  <div v-if="toggle">true</div>
+  <!-- <div v-if="toggle">true</div>
   <div v-else>false</div>
-  <button @click = "onToggle">Toggle</button>
+  <button @click = "onToggle">Toggle</button> -->
 
   <div class="container">
 
     <h2>To-Do List</h2>
 
-    <form
-      @submit.prevent = "onSubmit"
-    >
-      <div class="d-flex">
-        <!-- todo 입력창 -->
-        <div class="flex-grow-1 mr-2">
-          <input 
-            type="text" 
-            v-model = "todo"
-            class = "form-control"
-            placeholder="Type new to-do"
-          >
-        </div>
+    <!-- 폼 컴포넌트 -->
+    <TodoSimpleForm />
 
-        <!-- todo add버튼-->
-        <div>
-          <button 
-            type="submit"
-            class="btn btn-outline-primary" 
-          >
-            Add
-          </button>
-        </div>
 
-      </div>
-
-      <!--유저가 아무것도 안쓰고 추가를 할때마다 에러메세지를 띄우므로(자주 바뀌므로) v-if대신 v-show를 쓰는 것이 효율적! -->
-      <div 
-        v-show="hasError" 
-        style="color:red;"
-      >
-        Error! This field cannot be empty.
-      </div>
-      
-    </form>
 
     <!-- todo 리스트가 공란일 때 띄울 텍스트 -->
     <div v-if = "!todos.length">
@@ -95,8 +64,13 @@
 
 <script>
   import { ref } from 'vue'; // reactive는 객체나 배열에서만 사용 가능!
+  import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
   export default{
+    components : {
+      TodoSimpleForm
+    },
+
     setup(){
       const toggle = ref(false);
       const todo = ref( '' ); // 처음 input박스는 공란('')으로 설정
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <form
+      @submit.prevent = "onSubmit"
+    >
+      <div class="d-flex">
+        <!-- todo 입력창 -->
+        <div class="flex-grow-1 mr-2">
+          <input 
+            type="text" 
+            v-model = "todo"
+            class = "form-control"
+            placeholder="Type new to-do"
+          >
+        </div>
+
+        <!-- todo add버튼-->
+        <div>
+          <button 
+            type="submit"
+            class="btn btn-outline-primary" 
+          >
+            Add
+          </button>
+        </div>
+
+      </div>
+
+      <!--유저가 아무것도 안쓰고 추가를 할때마다 에러메세지를 띄우므로(자주 바뀌므로) v-if대신 v-show를 쓰는 것이 효율적! -->
+      <div 
+        v-show="hasError" 
+        style="color:red;"
+      >
+        Error! This field cannot be empty.
+      </div>
+      
+    </form>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
