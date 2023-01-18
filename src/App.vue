794d04d772770cf8acf754443450f34100d1b901
KimYuRa2 <ururur68@gmail.com>
2023-01-18 17:05:01 +0900

    42. Navbar 추가하기

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">To-do List</a>
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Todos <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </nav>
 
   <!-- 컴포넌트 -->
   <router-view/>
