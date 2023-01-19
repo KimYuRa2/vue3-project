9fa5dde5b68ab18b0b688773c3c8464727380214
KimYuRa2 <ururur68@gmail.com>
2023-01-19 15:11:33 +0900

    50. Todo 페이지 컴포넌트 만들기 3

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -3,7 +3,10 @@
   <div v-if="loading">
     Loading..
   </div>
-  <form v-else>
+  <form 
+    v-else
+    @submit.prevent="onSave"
+  >
     <div class="row">
         <div class="col-6">
             <div class="form-group">
@@ -61,11 +64,13 @@ export default {
         const router = useRouter(); // route정보 받아옴
         const todo = ref(null);
         const loading = ref(true); // todo를 null로 설정해둬서 처음 페이지 띄울 때 콘솔에 에러가 뜸 => getTodo함수 실행 후  todo.value = res.data;로 todo값 받아오면, form이 뜨도록 하기위한 설정!
+        const todoId = route.params.id;
+        
         console.log(route.params.id); // route.params.id = id정보 접근 가능
 
         /* id로 todo정보 db에 요청하고 받아오기 */
         const getTodo = async() => {
-            const res = await axios.get('http://localhost:3000/todos/' + route.params.id);
+            const res = await axios.get('http://localhost:3000/todos/' + todoId);
             console.log(res);
             todo.value = res.data; // res.data => {subject: 'dfasd', completed: false, id: 22}
             loading.value = false;
@@ -83,11 +88,22 @@ export default {
             })
         }
 
+        /* db에 todo변경 요청보내기 */
+        const onSave = async() => {
+            const res = await axios.put('http://localhost:3000/todos/' + todoId, {
+                subject : todo.value.subject,
+                completed : todo.value.completed
+            });
+
+            console.log(res);
+        }
+
         return{
             todo,
             loading,
             toggleTodoStatus,
             moveToTodoListPage,
+            onSave,
 
         }
 
