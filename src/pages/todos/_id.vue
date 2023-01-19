782143befcf70a1abf49bdc16554750fd5a605c9
KimYuRa2 <ururur68@gmail.com>
2023-01-19 14:57:55 +0900

    49. Todo 페이지 컴포넌트 만들기 2

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -4,22 +4,61 @@
     Loading..
   </div>
   <form v-else>
-    <div class="form-group">
-        <label>Todo Subject</label>
-        <input v-model="todo.subject" type="text" class="form-control"/>
+    <div class="row">
+        <div class="col-6">
+            <div class="form-group">
+                <label>Subject</label>
+                <input 
+                    v-model="todo.subject" 
+                    type="text" 
+                    class="form-control"
+                >
+            </div>
+        </div>
+
+        <!-- status -(completed 버튼 ) -->
+        <div class="col-6">
+            <div class="form-group">
+                <label>Status</label>
+                <div>
+                    <button 
+                        type="button"
+                        class="btn"
+                        :class= "todo.completed ? 'btn-success' : 'btn-danger' "    
+                        @click="toggleTodoStatus"
+                        >
+                        {{ todo.completed ? 'Completed' : 'Incomplete' }}
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
-    <button class="btn btn-primary">Save</button>
+    
+    <button 
+        type="submit" 
+        class="btn btn-primary"
+    >
+        Save
+    </button>
+
+    <button 
+        class="btn btn-outline-dark ml-2" 
+        @click="moveToTodoListPage"
+    >
+        Cancel
+    </button>
   </form>
 </template>
 
 <script>
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 import { ref } from '@vue/reactivity';
 
 export default {
     setup(){
         const route = useRoute(); // route정보 받아옴
+        const router = useRouter(); // route정보 받아옴
         const todo = ref(null);
         const loading = ref(true); // todo를 null로 설정해둬서 처음 페이지 띄울 때 콘솔에 에러가 뜸 => getTodo함수 실행 후  todo.value = res.data;로 todo값 받아오면, form이 뜨도록 하기위한 설정!
         console.log(route.params.id); // route.params.id = id정보 접근 가능
@@ -33,9 +72,22 @@ export default {
         }
         getTodo();
 
+        const toggleTodoStatus = () => {
+            todo.value.completed = !todo.value.completed;
+        }
+
+        const moveToTodoListPage = () => {
+            // useRouter 이용해서 TodoList페이지로 이동하기.
+            router.push({
+                name: 'Todos'
+            })
+        }
+
         return{
             todo,
             loading,
+            toggleTodoStatus,
+            moveToTodoListPage,
 
         }
 
