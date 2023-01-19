42d1b0fc67dc37b989c74a1890eac9d78809b8b7
KimYuRa2 <ururur68@gmail.com>
2023-01-19 21:05:30 +0900

    (Section 9. Toast 알림 컴포넌트 만들기) 53. toast 컴포넌트

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -52,6 +52,13 @@
         Cancel
     </button>
   </form>
+
+    <!-- Toast 컴포넌트 -->
+    <Toast 
+        v-if="showToast"
+        :message = "toastMessage"
+        :type="toastAlertType"
+    />
 </template>
 
 <script>
@@ -59,25 +66,39 @@ import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 import { ref, computed } from 'vue';
 import _ from 'lodash';
+import Toast from '@/components/Toast.vue';
 
 export default {
+    components: {
+        Toast,
+    },
     setup(){
         const route = useRoute(); // route정보 받아옴
         const router = useRouter(); // route정보 받아옴
         const todo = ref(null);
         const originalTodo = ref(null); // 수정전Todo
         const loading = ref(true); // todo를 null로 설정해둬서 처음 페이지 띄울 때 콘솔에 에러가 뜸 => getTodo함수 실행 후  todo.value = res.data;로 todo값 받아오면, form이 뜨도록 하기위한 설정!
+        const showToast = ref(false); // Toast
+        const toastMessage = ref(''); // Toast message
+        const toastAlertType = ref(''); // Toast
         const todoId = route.params.id;
+        
         
         console.log(route.params.id); // route.params.id = id정보 접근 가능
 
         /* id로 todo정보 db에 요청하고 받아오기 */
         const getTodo = async() => {
-            const res = await axios.get('http://localhost:3000/todos/' + todoId);
-            console.log(res);
-            todo.value = { ...res.data }; // res.data => {subject: 'dfasd', completed: false, id: 22}
-            originalTodo.value = { ...res.data };  // spread객체(...)로 만들어서 새로운 객체로 만들기.(todo와 originalTodo의 주소가 같으면 둘중 하나가 값을 변경될시 값이 같이 변경되기 때문. )
-            loading.value = false;
+            try{
+                const res = await axios.get('http://localhost:3000/todos/' + todoId);
+                console.log(res);
+                todo.value = { ...res.data }; // res.data => {subject: 'dfasd', completed: false, id: 22}
+                originalTodo.value = { ...res.data };  // spread객체(...)로 만들어서 새로운 객체로 만들기.(todo와 originalTodo의 주소가 같으면 둘중 하나가 값을 변경될시 값이 같이 변경되기 때문. )
+                loading.value = false;
+            } catch (err) {
+                tiggerToast('Something went wrong!', 'danger');
+                console.log(err);
+
+            }
         }
         getTodo();
 
@@ -92,15 +113,35 @@ export default {
             })
         }
 
+        /* Toast 띄우기 */
+        const tiggerToast = ( message, type='success' ) => {
+            toastMessage.value = message; // 받아온 message를 toastMessage에 저장. 
+            toastAlertType.value = type;
+            showToast.value = true;
+            
+            setTimeout( () => {
+                toastMessage.value = '';
+                toastAlertType.value = '';
+                showToast.value = false;
+            }, 3000);
+        }
+
         /* db에 todo변경 요청보내기 */
         const onSave = async() => {
-            const res = await axios.put('http://localhost:3000/todos/' + todoId, {
-                subject : todo.value.subject,
-                completed : todo.value.completed
-            });
+            try{
+                const res = await axios.put('http://localhost:3000/todos/' + todoId, {
+                    subject : todo.value.subject,
+                    completed : todo.value.completed
+                });
 
-            // console.log(res);
-            originalTodo.value = { ...res.data }; // 저장이 끝나고나면, save버튼을 다시 disabled시킨다.
+                // console.log(res);
+                originalTodo.value = { ...res.data }; // 저장이 끝나고나면, save버튼을 다시 disabled시킨다.
+                // Toast
+                tiggerToast('Successfully saved!'); // tiggerToast함수에 message('Successfully saved!') 보내기
+            } catch (err){
+                tiggerToast('Something went wrong!', 'danger');
+                console.log(err);
+            }
         }
 
         const todoUpdated = computed( () => {
@@ -114,6 +155,9 @@ export default {
             moveToTodoListPage,
             onSave,
             todoUpdated,
+            showToast,
+            toastMessage,
+            toastAlertType,
 
         }
 
