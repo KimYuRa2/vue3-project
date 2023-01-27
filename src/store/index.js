b32e7852897117feeb4521791f92b8edea22e2e3
KimYuRa2 <ururur68@gmail.com>
2023-01-27 21:02:29 +0900

    77. vuex mutations / actions

--- src/store/index.js
+++ src/store/index.js
@@ -7,5 +7,46 @@ export default createStore({
         toastMessage : '',// Toast message
         toastAlertType : '', 
         timeout : null
+    },
+    mutations : {
+        UPDATE_TOAST_MESSAGE (state, payload) { // state를 인자로 받아와서, state에 접근해서 state를 변경함 // payload : 원하는 데이터 받아옴
+            state.toastMessage = payload;
+        },
+        UPDATE_TOAST_ALERT_TYPE (state, payload) {
+            state.toastAlertType = payload;
+        },
+        UPDATE_TOAST_STATUS (state, payload){
+            state.showToast = payload;
+        },
+        UPDATE_TOAST_TIMEOUT (state, payload) {
+            state.timeout = payload;
+        }
+
+    },
+    actions : {
+        /* Toast 띄우기 */
+        tiggerToast ( { commit }, message, type='success' ) { // context객체 안의 commit
+            // toastMessage.value = message; // 받아온 message를 toastMessage에 저장. 
+            commit('UPDATE_TOAST_MESSAGE', message);// UPDATE_TOAST_MESSAGE mutation을 commit해주고, 데이터를 payload로 넘겨줌
+            
+            // toastAlertType.value = type;
+            commit('UPDATE_TOAST_ALERT_TYPE', type);
+
+            // showToast.value = true;
+            commit('UPDATE_TOAST_STATUS', true);
+            
+            timeout.value = setTimeout( () => {
+                /* 초기화 */
+                // toastMessage.value = '';
+                commit('UPDATE_TOAST_MESSAGE', '');
+
+                // toastAlertType.value = '';
+                commit('UPDATE_TOAST_ALERT_TYPE', '');
+
+                // showToast.value = false;
+                commit('UPDATE_TOAST_STATUS', false);
+
+            }, 3000);
+        }
     }
 })
