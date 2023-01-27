d4fb7b46a71d7a13592ca3ea293229b164d582ac
KimYuRa2 <ururur68@gmail.com>
2023-01-27 18:09:44 +0900

    71. useContext (정리본참고), 72. toRefs

---
+++ src/composables/count.js
@@ -0,0 +1,9 @@
+import { reactive, toRefs } from 'vue';
+
+export const useCount = () => {
+    const state = reactive ({ // 객체자체는 reactive한데, 그 안의 프로퍼티 값(count)는 reactive하지않음 => 그래서 toRefs 사용!
+        count:0
+    });
+
+    return toRefs(state); // convert to ref!!
+}
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>Home Page</div>
+  <div>{{ count }}</div>
+  <button @click="count++">Add</button>
 </template>
 
 <script>
-export default {
+import { useCount } from '@/composables/count'
 
+export default {
+  setup() {
+    const { count } = useCount(); // useCount()에서 state를 반환함
+
+    return {
+      count
+    }
+  }
 }
 </script>
 
